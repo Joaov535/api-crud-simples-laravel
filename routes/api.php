9900074900543da6d4357ff1fce30e1cee3a193d
Joaov535 <joaovictorcruz535@gmail.com>
2024-09-22 21:06:42 -0300

    rotas encapsuladas no middleware

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Models\Product;
+use App\Http\Middleware\BasicAuthentication;
 
-Route::get('/products',  [ProductController::class, 'list']);
-Route::get('/products/{id}',  [ProductController::class, 'listById']);
-Route::post('/products', [ProductController::class, 'create']);
-Route::put('/products', [ProductController::class, 'edit']);
-Route::delete('/products/{id}', [ProductController::class, 'delete']);
+Route::middleware([BasicAuthentication::class])->group(function () {
+    Route::get('/products',  [ProductController::class, 'list']);
+    Route::get('/products/{id}',  [ProductController::class, 'listById']);
+    Route::post('/products', [ProductController::class, 'create']);
+    Route::put('/products', [ProductController::class, 'edit']);
+    Route::delete('/products/{id}', [ProductController::class, 'delete']);
+});
